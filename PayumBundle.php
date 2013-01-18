f1aa86aff9da422e034077fec08880c214b55beb
Maksim Kotlyar <mkotlar@ukr.net>
2013-01-18 13:47:13 +0200

    Added PayPal pro payment

--- PayumBundle.php
+++ PayumBundle.php
@@ -3,8 +3,8 @@ namespace Payum\Bundle\PayumBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-
 use Payum\Bundle\PayumBundle\DependencyInjection\Factory\Payment\PaypalExpressCheckoutNvpPaymentFactory;
+use Payum\Bundle\PayumBundle\DependencyInjection\Factory\Payment\PaypalProCheckoutNvpPaymentFactory;
 use Payum\Bundle\PayumBundle\DependencyInjection\Factory\Payment\Be2BillPaymentFactory;
 use Payum\Bundle\PayumBundle\DependencyInjection\Factory\Storage\DoctrineStorageFactory;
 use Payum\Bundle\PayumBundle\DependencyInjection\Factory\Storage\FilesystemStorageFactory;
@@ -18,8 +18,9 @@ class PayumBundle extends Bundle
 
         /** @var $extension \Payum\Bundle\PayumBundle\DependencyInjection\PayumExtension */
         $extension = $container->getExtension('payum');
-        
+
         $extension->addPaymentFactory(new PaypalExpressCheckoutNvpPaymentFactory());
+        $extension->addPaymentFactory(new PaypalProCheckoutNvpPaymentFactory());
         $extension->addPaymentFactory(new Be2BillPaymentFactory());
         $extension->addPaymentFactory(new AuthorizeNetAimPaymentFactory());
 
