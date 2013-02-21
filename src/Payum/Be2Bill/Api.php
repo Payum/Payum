0c4d51eb56cfeec2600cc46a6fb4d76c28707ca8
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-02-21 22:03:56 +0200

    add other EXECCODEs.

--- src/Payum/Be2Bill/Api.php
+++ src/Payum/Be2Bill/Api.php
@@ -14,6 +14,94 @@ class Api
     
     const EXECCODE_SUCCESSFUL = '0000';
 
+    const EXECCODE_3DSECURE_IDENTIFICATION_REQUIRED = '0001';
+
+    const EXECCODE_PARAMETER_X_MISSING = '1001';
+
+    const EXECCODE_INVALID_PARAMETER_X = '1002';
+
+    const EXECCODE_HASH_ERROR = '1003';
+
+    const EXECCODE_UNSUPPORTED_PROTOCOL = '1004';
+
+    const EXECCODE_ALIAS_NOT_FOUND = '2001';
+
+    const EXECCODE_UNSUCCESSFUL_REFERENCE_TRANSACTION = '2002';
+
+    const EXECCODE_NON_REFUNDABLE_REFERENCE_TRANSACTION = '2003';
+
+    const EXECCODE_REFERENCE_TRANSACTION_NOT_FOUND = '2004';
+
+    const EXECCODE_NOT_ABLE_TO_CAPTURE_THE_REFERENCE_AUTHORIZATION = '2005';
+
+    const EXECCODE_UNFINISHED_REFERENCE_TRANSACTION = '2006';
+
+    const EXECCODE_INVALID_CAPTURE_AMOUNT = '2007';
+
+    const EXECCODE_ACCOUNT_DEACTIVATED = '3001';
+
+    const EXECCODE_UNAUTHORIZED_SERVER_IP_ADDRESS = '3002';
+
+    const EXECCODE_UNAUTHORIZED_TRANSACTION = '3003';
+
+    const EXECCODE_TRANSACTION_REFUSED_BY_THE_BANK = '4001';
+
+    const EXECCODE_INSUFFICIENT_FUNDS = '4002';
+
+    const EXECCODE_CARD_REFUSED_BY_THE_BANK = '4003';
+
+    const EXECCODE_ABORTED_TRANSACTION = '4004';
+    
+    const EXECCODE_SUSPECTED_FRAUD = '4005';
+
+    const EXECCODE_CARD_LOST = '4006';
+
+    const EXECCODE_STOLEN_CARD = '4007';
+
+    const EXECCODE_3DSECURE_AUTHENTICATION_FAILED = '4008';
+
+    const EXECCODE_EXPIRED_3DSECURE_AUTHENTICATION = '4009';
+
+    const EXECCODE_INTERNAL_ERROR = '5001';
+
+    const EXECCODE_BANK_ERROR = '5002';
+
+    const EXECCODE_UNDERGOING_MAINTENANCE = '5003';
+
+    const EXECCODE_TIME_OUT = '5004';
+
+    /**
+     * The "payment" function is the basic function that allows collecting from a cardholder. 
+     * This operation collects money directly.
+     */
+    const OPERATION_PAYMENT = 'payment';
+
+    /**
+     * The "authorization" function allows "freezing" temporarily the funds in a cardholder's bank
+     * account for 7 days. This application does not debit it.
+     * This type of operation is mainly used in the world of physical goods ("retail") when the merchant
+     * decides to debit his customer at merchandise shipping time.
+     */
+    const OPERATION_AUTHORIZATION = 'authorization';
+
+    /**
+     * The "capture" function allows collecting funds from a cardholder after an authorization
+     * ("authorization" function). This capture can take place within 7 days after the authorization.
+     */
+    const OPERATION_CAPTURE = 'capture';
+
+    /**
+     * This dual function is directly managed by the system:
+     * - Refund: Consists of returning the already collected funds to a cardholder
+     * - Cancellation: Consists of not sending a payment transaction as compensation
+     */
+    const OPERATION_REFUND = 'refund';
+
+    /**
+     * The "credit" function allows sending funds to a cardholder.
+     */
+    const OPERATION_CREDIT = 'credit';
+
     /**
      * @var \Buzz\Client\ClientInterface
      */
@@ -61,7 +149,7 @@ class Api
 
         $params['VERSION'] = self::VERSION;
         $params['IDENTIFIER'] = $this->options['identifier'];
-        $params['OPERATIONTYPE'] = 'payment';
+        $params['OPERATIONTYPE'] = static::OPERATION_PAYMENT;
         $params['HASH'] = $this->calculateHash($params);
         
         $request->setField('method', 'payment');
@@ -117,5 +205,4 @@ class Api
 
         return hash('sha256', $clearString);
     }
-    
 }
