3ad1b64f31052624805ec9c134a58b50600edd99
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-02-27 10:10:34 +0200

    fix test.

--- tests/Payum/OmnipayBridge/Tests/Integration/PaymentTest.php
+++ tests/Payum/OmnipayBridge/Tests/Integration/PaymentTest.php
@@ -15,13 +15,15 @@ class PaymentTest extends \PHPUnit_Framework_TestCase
     {
         $payment = PaymentFactory::create(new Gateway());
 
+        $date = new \DateTime('now + 2 year');
+        
         $captureRequest = new CaptureRequest(array(
             'amount' => 1000,
             'card' => array(
                 'number' => '5555556778250000', //end zero so will be accepted
                 'cvv' => 123,
                 'expiryMonth' => 6,
-                'expiryYear' => (new \DateTime('now + 2 year'))->format('y'),
+                'expiryYear' => $date->format('y'),
                 'firstName' => 'foo',
                 'lastName' => 'bar',
             )
