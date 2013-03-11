36e215ad728a7a8cace51f29096c6877e42ba8b3
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-03-11 21:41:00 +0200

    add payment instruct aggregate actions.

--- src/Payum/AuthorizeNet/Aim/PaymentFactory.php
+++ src/Payum/AuthorizeNet/Aim/PaymentFactory.php
@@ -3,6 +3,9 @@ namespace Payum\AuthorizeNet\Aim;
 
 use Payum\Payment;
 use Payum\Extension\EndlessCycleDetectorExtension;
+use Payum\Action\CapturePaymentInstructionAggregateAction;
+use Payum\Action\SyncPaymentInstructionAggregateAction;
+use Payum\Action\StatusPaymentInstructionAggregateAction;
 use Payum\AuthorizeNet\Aim\Bridge\AuthorizeNet\AuthorizeNetAIM;
 use Payum\AuthorizeNet\Aim\Action\CaptureAction;
 use Payum\AuthorizeNet\Aim\Action\StatusAction;
@@ -22,8 +25,11 @@ abstract class PaymentFactory
         
         $payment->addExtension(new EndlessCycleDetectorExtension);
 
-        $payment->addAction(new CaptureAction());
-        $payment->addAction(new StatusAction());
+        $payment->addAction(new CaptureAction);
+        $payment->addAction(new StatusAction);
+        $payment->addAction(new CapturePaymentInstructionAggregateAction);
+        $payment->addAction(new SyncPaymentInstructionAggregateAction);
+        $payment->addAction(new StatusPaymentInstructionAggregateAction);
 
         return $payment;
     }
