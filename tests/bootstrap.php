939ecff3e8b34cd2b70479e7cc7ffec52eb7b00d
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-04-03 22:11:18 +0300

    add test bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    echo <<<EOM
+You must set up the project dependencies by running the following commands:
+
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+
+EOM;
+
+    exit(1);
+}
+
+$loader->add('Payum\Paypal\Ipn\Tests', __DIR__);
