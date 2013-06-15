7ce27656f38eaedb62e2ebd276e8ff841bfeb9cc
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-06-15 17:25:22 +0300

    add agreement actions and api to payment factory.

--- src/Payum/Payex/PaymentFactory.php
+++ src/Payum/Payex/PaymentFactory.php
@@ -1,25 +1,39 @@
 <?php
 namespace Payum\Payex;
 
+use Payum\Payment;
 use Payum\Extension\EndlessCycleDetectorExtension;
+use Payum\Payex\Action\Api\AutoPayAgreementAction;
+use Payum\Payex\Action\Api\CheckAgreementAction;
 use Payum\Payex\Action\Api\CompleteOrderAction;
+use Payum\Payex\Action\Api\CreateAgreementAction;
+use Payum\Payex\Action\Api\DeleteAgreementAction;
 use Payum\Payex\Action\Api\InitializeOrderAction;
 use Payum\Payex\Action\CaptureAction;
-use Payum\Payex\Action\StatusAction;
+use Payum\Payex\Action\PaymentDetailsStatusAction;
+use Payum\Payex\Api\AgreementApi;
 use Payum\Payex\Api\OrderApi;
-use Payum\Payex\Request\Api\CompleteOrderRequest;
-use Payum\Payment;
 
 abstract class PaymentFactory
 {
     /**
      * @param Api\OrderApi $orderApi
-     *
+     * @param Api\AgreementApi $agreementApi
+     * 
      * @return \Payum\Payment
      */
-    public static function create(OrderApi $orderApi)
+    public static function create(OrderApi $orderApi, AgreementApi $agreementApi = null)
     {
         $payment = new Payment;
+        
+        if ($agreementApi) {
+            $payment->addApi($agreementApi);
+            
+            $payment->addAction(new CreateAgreementAction);
+            $payment->addAction(new DeleteAgreementAction);
+            $payment->addAction(new CheckAgreementAction);
+            $payment->addAction(new AutoPayAgreementAction);
+        }
 
         $payment->addApi($orderApi);
 
@@ -29,7 +43,7 @@ abstract class PaymentFactory
         $payment->addAction(new CompleteOrderAction);
         
         $payment->addAction(new CaptureAction);
-        $payment->addAction(new StatusAction);
+        $payment->addAction(new PaymentDetailsStatusAction);
 
         return $payment;
     }
--- tests/Payum/Payex/Tests/PaymentFactoryTest.php
+++ tests/Payum/Payex/Tests/PaymentFactoryTest.php
@@ -3,6 +3,7 @@ namespace Payum\Payex\Tests;
 
 use Payum\Payex\PaymentFactory;
 use Payum\Payex\Api\OrderApi;
+use Payum\Payex\Api\AgreementApi;
 
 class PaymentFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -31,7 +32,26 @@ class PaymentFactoryTest extends \PHPUnit_Framework_TestCase
 
         $actions = $this->readAttribute($payment, 'actions');
         $this->assertInternalType('array', $actions);
-        $this->assertNotEmpty($actions);
+        $this->assertCount(4, $actions);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowCreatePaymentWithStandardActionsAddedPlusAgreementOnes()
+    {
+        $orderApiMock = $this->createOrderApiMock();
+        $agreementApiMock = $this->createAgreementApiMock();
+
+        $payment = PaymentFactory::create($orderApiMock, $agreementApiMock);
+
+        $this->assertInstanceOf('Payum\Payment', $payment);
+
+        $this->assertAttributeCount(2, 'apis', $payment);
+
+        $actions = $this->readAttribute($payment, 'actions');
+        $this->assertInternalType('array', $actions);
+        $this->assertCount(8, $actions);
     }
 
     /**
@@ -40,5 +60,13 @@ class PaymentFactoryTest extends \PHPUnit_Framework_TestCase
     protected function createOrderApiMock()
     {
         return $this->getMock('Payum\Payex\Api\OrderApi', array(), array(), '', false);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|AgreementApi
+     */
+    protected function createAgreementApiMock()
+    {
+        return $this->getMock('Payum\Payex\Api\AgreementApi', array(), array(), '', false);
     }
 }
