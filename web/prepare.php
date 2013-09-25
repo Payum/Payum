358b8b32d3ebd46126aa9068026f2a4bdc894fed
skadabr <poljakov.ws@gmail.com>
2013-09-25 11:07:03 +0300

    work version 1

--- web/prepare.php
+++ web/prepare.php
@@ -12,13 +12,13 @@ $storage->updateModel($paymentDetails);
 $doneToken = $tokenStorage->createModel();
 $doneToken->setPaymentName('paypal');
 $doneToken->setDetails($storage->getIdentificator($paymentDetails));
-$doneToken->setTargetUrl('/done.php?payum_token='.$doneToken->getHash());
+$doneToken->setTargetUrl('http://'.$_SERVER['HTTP_HOST'].'/done.php?payum_token='.$doneToken->getHash());
 $tokenStorage->updateModel($doneToken);
 
 $captureToken = $tokenStorage->createModel();
 $captureToken->setPaymentName('paypal');
 $captureToken->setDetails($storage->getIdentificator($paymentDetails));
-$captureToken->setTargetUrl('/capture.php?payum_token='.$captureToken->getHash());
+$captureToken->setTargetUrl('http://'.$_SERVER['HTTP_HOST'].'/capture.php?payum_token='.$captureToken->getHash());
 $captureToken->setAfterUrl($doneToken->getTargetUrl());
 $tokenStorage->updateModel($captureToken);
 
