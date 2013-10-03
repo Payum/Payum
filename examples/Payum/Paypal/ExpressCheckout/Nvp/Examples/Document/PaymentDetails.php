063d52cc63ba952cb506cbd7c905fae2adcfb027
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-10-03 11:48:58 +0300

    working base mongo tests config. still  have problem with hash fields.

--- examples/Payum/Paypal/ExpressCheckout/Nvp/Examples/Document/PaymentDetails.php
+++ examples/Payum/Paypal/ExpressCheckout/Nvp/Examples/Document/PaymentDetails.php
@@ -2,7 +2,7 @@
 namespace Payum\Paypal\ExpressCheckout\Nvp\Examples\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as Mongo;
-use Payum\Paypal\ExpressCheckout\Nvp\Bridge\Doctrine\Entity\PaymentDetails as BasePaymentDetails;
+use Payum\Paypal\ExpressCheckout\Nvp\Bridge\Doctrine\Document\PaymentDetails as BasePaymentDetails;
 
 /**
  * @Mongo\Document
