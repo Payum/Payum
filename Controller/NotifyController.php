018304b77ece78453fbad799861b1975d872e725
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-10-25 22:31:45 +0300

    remove yearly deprecated code.

--- Controller/NotifyController.php
+++ Controller/NotifyController.php
@@ -20,14 +20,4 @@ class NotifyController extends PayumController
 
         return new Response('', 204);
     }
-
-    /**
-     * @deprecated since 0.6 will be removed in 0.7. This route present for easy migration from 0.5 version.
-     */
-    public function doDeprecatedAction(Request $request)
-    {
-        return $this->forward('Payum:Notify:do', array(
-            'payum_token' => $request->attributes->get('payumToken', $request->get('payumToken'))
-        ));
-    }
 }
