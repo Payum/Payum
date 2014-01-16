14a299cdda6c5dcd3c2e3d20234d952b8b6249d3
Alexandre Bacco <alexandre.bacco@gmail.com>
2014-01-16 17:27:50 +0800

    Add a method to test a hash(cherry picked from commit bbc3577cd9f656d7d45354969b966448c31c4767)

--- src/Payum/Be2Bill/Api.php
+++ src/Payum/Be2Bill/Api.php
@@ -155,6 +155,25 @@ class Api
 
         return $this->doRequest($request);
     }
+    
+    /**
+     * Verify if the hash of the given parameter is correct
+     * 
+     * @param array $params
+     * 
+     * @return bool
+     */
+    public function verifyHash(array $params)
+    {
+        if (empty($params['HASH'])) {
+            return false;
+        }
+        
+        $hash = $params['HASH'];
+        unset($params['HASH']);
+        
+        return $hash === $this->calculateHash($params);
+    }
 
     /**
      * @param \Buzz\Message\Form\FormRequest $request
