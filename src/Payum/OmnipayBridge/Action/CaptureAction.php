2ade68323fa13d4d149259a2933e3ea1d8e205d8
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2014-03-11 10:18:39 +0200

    [omnipay] do not wrap omnipay's exceptions in capture action.

--- src/Payum/OmnipayBridge/Action/CaptureAction.php
+++ src/Payum/OmnipayBridge/Action/CaptureAction.php
@@ -21,33 +21,23 @@ class CaptureAction extends BaseApiAwareAction
             throw RequestNotSupportedException::createActionNotSupported($this, $request);
         }
 
-        try {
-            $options = ArrayObject::ensureArrayObject($request->getModel());
+        $options = ArrayObject::ensureArrayObject($request->getModel());
 
-            if (isset($options['_completeCaptureRequired'])) {
-                unset($options['_completeCaptureRequired']);
-                $response = $this->gateway->completePurchase($options->toUnsafeArray())->send();
-            } else {
-                $response = $this->gateway->purchase($options->toUnsafeArray())->send();
-            }
-
-            if ($response->isRedirect()) {
-                throw new RedirectUrlInteractiveRequest($response->getRedirectUrl());
-            }
-
-            $options['_reference']      = $response->getTransactionReference();
-            $options['_status']         = $response->isSuccessful() ? 'success' : 'failed';
-            $options['_status_code']    = $response->getCode();
-            $options['_status_message'] = $response->isSuccessful() ? '' : $response->getMessage();
-        } catch (InteractiveRequestInterface $e) {
-            $options['_completeCaptureRequired'] = 1;
-
-            throw $e;
-        } catch (\Exception $e) {
-            $options['_status'] = 'failed';
-
-            throw new LogicException('Omnipay unexpected exception', null, $e);
+        if (isset($options['_completeCaptureRequired'])) {
+            unset($options['_completeCaptureRequired']);
+            $response = $this->gateway->completePurchase($options->toUnsafeArray())->send();
+        } else {
+            $response = $this->gateway->purchase($options->toUnsafeArray())->send();
         }
+
+        if ($response->isRedirect()) {
+            throw new RedirectUrlInteractiveRequest($response->getRedirectUrl());
+        }
+
+        $options['_reference']      = $response->getTransactionReference();
+        $options['_status']         = $response->isSuccessful() ? 'success' : 'failed';
+        $options['_status_code']    = $response->getCode();
+        $options['_status_message'] = $response->isSuccessful() ? '' : $response->getMessage();
     }
 
     /**
