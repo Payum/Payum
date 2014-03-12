ac3c9059dca9fd24b3e927f30590e5c1d2ff109b
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-03-12 14:41:17 +0000

    [paypal][pro] set bigger timeout to prevent test fails

--- src/Payum/Paypal/ProCheckout/Nvp/Tests/Functional/ExecuteTest.php
+++ src/Payum/Paypal/ProCheckout/Nvp/Tests/Functional/ExecuteTest.php
@@ -10,59 +10,63 @@ use Payum\Core\Request\CaptureRequest;
 
 class ExecuteTest extends \PHPUnit_Framework_TestCase
 {
-  /**
-   * @test
-   */
-  public function doCapture()
-  {
-    if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_USERNAME'])) {
-      $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_USERNAME in your phpunit.xml');
+    /**
+    * @test
+    */
+    public function doCapture()
+    {
+        if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_USERNAME'])) {
+            $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_USERNAME in your phpunit.xml');
+        }
+        if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PASSWORD'])) {
+            $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PASSWORD in your phpunit.xml');
+        }
+        if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PARTNER'])) {
+            $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PARTNER in your phpunit.xml');
+        }
+        if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_VENDOR'])) {
+            $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_VENDOR in your phpunit.xml');
+        }
+
+
+
+        //@testo:start
+        $client = new Curl;
+        $client->setTimeout(20);
+
+        $payment = PaymentFactory::create(new Api($client, array(
+            'username' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_USERNAME'],
+            'password' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PASSWORD'],
+            'partner' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PARTNER'],
+            'vendor' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_VENDOR'],
+            'sandbox' => true
+        )));
+
+        $instruction = new PaymentDetails();
+        $instruction->setCurrency('USD');
+        $instruction->setAmt('1.00');
+        $instruction->setAcct('5105105105105100');
+        $instruction->setExpDate('1214');
+        $instruction->setCvv2('123');
+        $instruction->setBillToFirstName('John');
+        $instruction->setBillToLastName('Doe');
+        $instruction->setBillToStreet('123 Main St.');
+        $instruction->setBillToCity('San Jose');
+        $instruction->setBillToState('CA');
+        $instruction->setBillToZip('95101');
+        $instruction->setBillToCountry('US');
+
+        $captureRequest = new CaptureRequest($instruction);
+        $payment->execute($captureRequest);
+
+        $statusRequest = new BinaryMaskStatusRequest($captureRequest->getModel());
+        $payment->execute($statusRequest);
+
+        $response = $instruction->getResponse();
+        // ...
+        //@testo:end
+        $this->assertNotEmpty($response);
+        $this->assertArrayHasKey('RESULT', $response);
+        $this->assertEquals(Api::RESULT_SUCCESS, $response['RESULT']);
     }
-    if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PASSWORD'])) {
-      $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PASSWORD in your phpunit.xml');
-    }
-    if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PARTNER'])) {
-      $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PARTNER in your phpunit.xml');
-    }
-    if (false == isset($GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_VENDOR'])) {
-      $this->markTestSkipped('Please configure __PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_VENDOR in your phpunit.xml');
-    }
-
-
-    //@testo:start
-    $payment = PaymentFactory::create(new Api(new Curl, array(
-        'username' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_USERNAME'],
-        'password' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PASSWORD'],
-        'partner' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_PARTNER'],
-        'vendor' => $GLOBALS['__PAYUM_PAYPAL_PRO_CHECKOUT_NVP_API_VENDOR'],
-        'sandbox' => true
-    )));
-
-    $instruction = new PaymentDetails();
-    $instruction->setCurrency('USD');
-    $instruction->setAmt('1.00');
-    $instruction->setAcct('5105105105105100');
-    $instruction->setExpDate('1214');
-    $instruction->setCvv2('123');
-    $instruction->setBillToFirstName('John');
-    $instruction->setBillToLastName('Doe');
-    $instruction->setBillToStreet('123 Main St.');
-    $instruction->setBillToCity('San Jose');
-    $instruction->setBillToState('CA');
-    $instruction->setBillToZip('95101');
-    $instruction->setBillToCountry('US');
-
-    $captureRequest = new CaptureRequest($instruction);
-    $payment->execute($captureRequest);
-
-    $statusRequest = new BinaryMaskStatusRequest($captureRequest->getModel());
-    $payment->execute($statusRequest);
-
-    $response = $instruction->getResponse();
-    // ...
-    //@testo:end
-    $this->assertNotEmpty($response);
-    $this->assertArrayHasKey('RESULT', $response);
-    $this->assertEquals(Api::RESULT_SUCCESS, $response['RESULT']);
-  }
 }
