6c1111855e972b10786f8394b2113d2f9ac9f0a4
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-05-28 19:18:55 +0000

    [security] added more test for mask util, support of utf, short strings.

--- src/Payum/Core/Tests/Security/Util/MaskTest.php
+++ src/Payum/Core/Tests/Security/Util/MaskTest.php
@@ -8,20 +8,26 @@ class MaskTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function provideCreditCardToMask()
+    public function provideValues()
     {
         return array(
-            'just numbers 16' => array("4567890123456789", "4XXXXXXXXXXX6789"),
-            'just numbers 9' => array("498291842", "4XXXX1842"),
-            'with dash' => array("3456-7890-1234-5678", "3XXX-XXXX-XXXX-5678"),
-            'with a-z' => array("4928-abcd9012-3456", "4XXX-XXXXXXXX-3456"),
+            'just 16 numbers' => array("4567890123456789", "4XXXXXXXXXXX6789"),
+            'just 9 numbers' => array("498291842", "4XXXX1842"),
+            'numbers with dash' => array("3456-7890-1234-5678", "3XXX-XXXX-XXXX-5678"),
+            'numbers with a-z' => array("4928-abcd9012-3456", "4XXX-XXXXXXXX-3456"),
+            'english full name' => array("Mr. John Doe", "MXX XXXX Doe"),
+            'german full name' => array("Günther Doe", "GXXXXXX Doe"),
+            'russian full name' => array("Иван Петров", "ИXXX XXтров"),
+            'short name' => array("Bea", "BXX"),
+            'short name edge case' => array('Barbara', 'BXXXXXX'),
+            'short name that masked' => array('Beatrices', 'BXXXXices'),
         );
     }
 
     /**
      * @test
      *
-     * @dataProvider provideCreditCardToMask
+     * @dataProvider provideValues
      */
     public function shouldAllowGenerateToken($value, $expected)
     {
@@ -43,4 +49,12 @@ class MaskTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertSame('1XXX-XXXX-1234-5678', Mask::mask('1234-5678-1234-5678', null, 8));
     }
-} 
+
+    /**
+     * @test
+     */
+    public function shouldNotShowAnythingIfNegativeShowLastGiven()
+    {
+        $this->assertSame('BXXXXXXXX', Mask::mask('Beatrices', null, -1));
+    }
+}
