6c4a33e78293efe5fe9daab5c8c7c2d9d10fd869
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-05-30 19:28:40 +0000

    allow choose secure or not credit card should be.

--- src/Payum/Core/Model/CreditCardInterface.php
+++ src/Payum/Core/Model/CreditCardInterface.php
@@ -82,4 +82,9 @@ interface CreditCardInterface
      * @param \DateTime $date
      */
     public function setExpireAt(\DateTime $date);
+
+    /**
+     * Wraps all sensitive values by SensitiveValue objects. Prevent accidental storing of them while serialization and so on.
+     */
+    public function secure();
 }
