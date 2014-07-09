4c91b70a8a59708a0b1d522f568c99c38fc99ea2
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-07-09 17:38:31 +0000

    [stripe] finish tests for obtain token action  class

--- src/Payum/Stripe/Tests/Action/Api/ObtainTokenActionTest.php
+++ src/Payum/Stripe/Tests/Action/Api/ObtainTokenActionTest.php
@@ -2,7 +2,9 @@
 namespace Payum\Stripe\Tests\Action\Api;
 
 use Payum\Core\PaymentInterface;
+use Payum\Core\Request\Http\GetRequestRequest;
 use Payum\Core\Request\Http\ResponseInteractiveRequest;
+use Payum\Core\Request\RenderTemplateRequest;
 use Payum\Stripe\Action\Api\ObtainTokenAction;
 use Payum\Stripe\Keys;
 use Payum\Stripe\Request\Api\ObtainTokenRequest;
@@ -107,22 +109,120 @@ class ObtainTokenActionTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function shouldRenderExpectedPageIfGetRequest()
+    public function shouldRenderExpectedTemplateIfHttpRequestNotPOST()
     {
-        $action = new ObtainTokenAction('aTemplateName');
-        $action->setPayment($this->createPaymentMock());
-        $action->setApi(new Keys('publishableKey', 'secretKey'));
+        $model = array();
+        $templateName = 'theTemplateName';
+        $publishableKey ='thePubKey';
+
+        $testCase = $this;
+
+        $paymentMock = $this->createPaymentMock();
+        $paymentMock
+            ->expects($this->at(0))
+            ->method('execute')
+            ->with($this->isInstanceOf('Payum\Core\Request\Http\GetRequestRequest'))
+            ->will($this->returnCallback(function(GetRequestRequest $request) {
+                $request->method = 'GET';
+            }))
+        ;
+        $paymentMock
+            ->expects($this->at(1))
+            ->method('execute')
+            ->with($this->isInstanceOf('Payum\Core\Request\RenderTemplateRequest'))
+            ->will($this->returnCallback(function(RenderTemplateRequest $request) use ($templateName, $publishableKey, $model, $testCase) {
+                $testCase->assertEquals($templateName, $request->getTemplateName());
+
+                $context = $request->getContext();
+                $testCase->assertArrayHasKey('model', $context);
+                $testCase->assertArrayHasKey('publishable_key', $context);
+                $testCase->assertEquals($publishableKey, $context['publishable_key']);
+
+                $request->setResult('theContent');
+            }))
+        ;
+
+        $action = new ObtainTokenAction($templateName);
+        $action->setPayment($paymentMock);
+        $action->setApi(new Keys($publishableKey, 'secretKey'));
 
         try {
-            $action->execute(new ObtainTokenRequest(array()));
+            $action->execute(new ObtainTokenRequest($model));
         } catch (ResponseInteractiveRequest $interactiveRequest) {
-
+            $this->assertEquals('theContent', $interactiveRequest->getContent());
 
             return;
         }
 
 
         $this->fail('Response interactive request was expected to be thrown.');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRenderTemplateIfHttpRequestPOSTButNotContainStripeToken()
+    {
+        $model = array();
+        $templateName = 'aTemplateName';
+        $publishableKey ='aPubKey';
+
+        $paymentMock = $this->createPaymentMock();
+        $paymentMock
+            ->expects($this->at(0))
+            ->method('execute')
+            ->with($this->isInstanceOf('Payum\Core\Request\Http\GetRequestRequest'))
+            ->will($this->returnCallback(function(GetRequestRequest $request) {
+                $request->method = 'POST';
+            }))
+        ;
+        $paymentMock
+            ->expects($this->at(1))
+            ->method('execute')
+            ->with($this->isInstanceOf('Payum\Core\Request\RenderTemplateRequest'))
+        ;
+
+        $action = new ObtainTokenAction($templateName);
+        $action->setPayment($paymentMock);
+        $action->setApi(new Keys($publishableKey, 'secretKey'));
+
+        try {
+            $action->execute(new ObtainTokenRequest($model));
+        } catch (ResponseInteractiveRequest $interactiveRequest) {
+            return;
+        }
+
+        $this->fail('Response interactive request was expected to be thrown.');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetTokenFromHttpRequestToObtainTokenRequestOnPOST()
+    {
+        $model = array();
+        $templateName = 'aTemplateName';
+        $publishableKey ='aPubKey';
+
+        $paymentMock = $this->createPaymentMock();
+        $paymentMock
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->isInstanceOf('Payum\Core\Request\Http\GetRequestRequest'))
+            ->will($this->returnCallback(function(GetRequestRequest $request) {
+                $request->method = 'POST';
+                $request->request = array('stripeToken' => 'theToken');
+            }))
+        ;
+
+        $action = new ObtainTokenAction($templateName);
+        $action->setPayment($paymentMock);
+        $action->setApi(new Keys($publishableKey, 'secretKey'));
+
+        $action->execute($obtainTokenRequest = new ObtainTokenRequest($model));
+
+        $model = $obtainTokenRequest->getModel();
+        $this->assertEquals('theToken', $model['card']);
     }
 
     /**
