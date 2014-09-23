711299a58dd56a4960fc3efbfa1a0cdc12d00096
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-09-23 12:18:54 +0000

    [klarna] add CreditPartAction.

---
+++ src/Payum/Klarna/Invoice/Tests/Request/Api/CreditPartTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Payum\Klarna\Invoice\Tests\Request\Api;
+
+use Payum\Klarna\Invoice\Request\Api\CreditPart;
+
+class CreditPartTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldBeSubClassOfBaseOrder()
+    {
+        $rc = new \ReflectionClass('Payum\Klarna\Invoice\Request\Api\CreditPart');
+
+        $this->assertTrue($rc->isSubclassOf('Payum\Core\Request\BaseModelAware'));
+    }
+
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithArrayModelAsArgument()
+    {
+        new CreditPart(array());
+    }
+}
