e0cc252d59672afe78f4d76ff03fea9f6b34621d
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-09-23 20:19:32 +0000

    [request] add refund request

---
+++ src/Payum/Core/Tests/Request/RefundTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Payum\Core\Tests\Request;
+
+use Payum\Core\Request\Refund;
+
+class RefundTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldBeSubClassOfGeneric()
+    {
+        $rc = new \ReflectionClass('Payum\Core\Request\Refund');
+        
+        $this->assertTrue($rc->isSubclassOf('Payum\Core\Request\Generic'));
+    }
+
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithModel()
+    {
+        new Refund(new \stdClass);
+    }
+}
