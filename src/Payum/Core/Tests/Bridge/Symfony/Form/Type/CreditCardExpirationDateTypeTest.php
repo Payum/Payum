fdc5925cae9f5af5cd2e5251d675a29e571b4359
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-10-12 14:02:01 +0000

    [bridge][symfony] add obtain credit card action and credit card form.

---
+++ src/Payum/Core/Tests/Bridge/Symfony/Form/Type/CreditCardExpirationDateTypeTest.php
@@ -0,0 +1,64 @@
+<?php
+namespace Payum\Core\Tests\Bridge\Symfony\Form\Type;
+
+use Payum\Core\Bridge\Symfony\Form\Type\CreditCardExpirationDateType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreditCardExpirationDateTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldBeSubClassOfAbstractType()
+    {
+        $rc = new \ReflectionClass('Payum\Core\Bridge\Symfony\Form\Type\CreditCardExpirationDateType');
+
+        $this->assertTrue($rc->isSubclassOf('Symfony\Component\Form\AbstractType'));
+    }
+
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithoutAnyArguments()
+    {
+        new CreditCardExpirationDateType;
+    }
+
+    /**
+     * @test
+     */
+    public function shouldExtendDateType()
+    {
+        $type = new CreditCardExpirationDateType;
+
+        $this->assertEquals('date', $type->getParent());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnExpectedName()
+    {
+        $type = new CreditCardExpirationDateType;
+
+        $this->assertEquals('payum_credit_card_expiration_date', $type->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowResolveOptions()
+    {
+        $type = new CreditCardExpirationDateType;
+
+        $resolver = new OptionsResolver;
+
+        $type->setDefaultOptions($resolver);
+
+        $options = $resolver->resolve();
+
+        $this->assertArrayHasKey('years', $options);
+        $this->assertCount(10, $options['years']);
+    }
+
+}
