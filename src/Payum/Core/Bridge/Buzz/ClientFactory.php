cfe388ecd1953a96af56fcda5aaf623eb1ba21b1
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-11-07 20:16:02 +0000

    fix consstant CURL_SSLVERSION_TLSv1 does not exists.

--- src/Payum/Core/Bridge/Buzz/ClientFactory.php
+++ src/Payum/Core/Bridge/Buzz/ClientFactory.php
@@ -16,7 +16,9 @@ class ClientFactory
         //https://www.paypal-community.com/t5/PayPal-Forward/PayPal-Response-to-SSL-3-0-Vulnerability-aka-POODLE/ba-p/891829
         //http://googleonlinesecurity.blogspot.com/2014/10/this-poodle-bites-exploiting-ssl-30.html
         $client->setOption(CURLOPT_SSL_CIPHER_LIST, 'TLSv1');
-        $client->setOption(CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
+
+        //There is a constant for that CURL_SSLVERSION_TLSv1, but it is not present on some versions of php.
+        $client->setOption(CURLOPT_SSLVERSION, $curlSslVersionTlsV1 = 1);
 
         return $client;
     }
