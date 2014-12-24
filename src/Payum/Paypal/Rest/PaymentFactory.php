b13810f887c31ea34e3017686abf2e8fa05e10f0
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2014-12-24 13:46:40 +0000

    [Paypal rest] updated payment factory.

--- src/Payum/Paypal/Rest/PaymentFactory.php
+++ src/Payum/Paypal/Rest/PaymentFactory.php
@@ -1,43 +1,63 @@
 <?php
 namespace Payum\Paypal\Rest;
 
-use Payum\Core\Action\ExecuteSameRequestWithModelDetailsAction;
-use Payum\Core\Action\GetHttpRequestAction;
-use Payum\Core\Payment;
-use Payum\Core\Extension\EndlessCycleDetectorExtension;
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\PaymentFactory as CorePaymentFactory;
+use Payum\Core\PaymentFactoryInterface;
 use Payum\Paypal\Rest\Action\CaptureAction;
 use Payum\Paypal\Rest\Action\StatusAction;
-use PayPal\Rest\ApiContext;
 use Payum\Paypal\Rest\Action\SyncAction;
 
-abstract class PaymentFactory
+class PaymentFactory implements PaymentFactoryInterface
 {
     /**
-     * @param ApiContext $api
-     *
-     * @return \Payum\Core\Payment
+     * @var PaymentFactoryInterface
      */
-    public static function create(ApiContext $api)
+    protected $corePaymentFactory;
+
+    /**
+     * @param PaymentFactoryInterface $corePaymentFactory
+     */
+    public function __construct(PaymentFactoryInterface $corePaymentFactory = null)
     {
-        $payment = new Payment();
-
-        $payment->addApi($api);
-
-        $payment->addExtension(new EndlessCycleDetectorExtension());
-
-        $payment->addAction(new CaptureAction());
-        $payment->addAction(new StatusAction());
-        $payment->addAction(new SyncAction());
-        $payment->addAction(new GetHttpRequestAction());
-
-        $payment->addAction(new ExecuteSameRequestWithModelDetailsAction());
-
-        return $payment;
+        $this->corePaymentFactory = $corePaymentFactory ?: new CorePaymentFactory();
     }
 
     /**
+     * {@inheritDoc}
      */
-    private function __construct()
+    public function create(array $config = array())
     {
+        return $this->corePaymentFactory->create($this->createConfig($config));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createConfig(array $config = array())
+    {
+        $config = ArrayObject::ensureArrayObject($config);
+        $config->defaults($this->corePaymentFactory->createConfig());
+
+        $config->defaults(array(
+            'payum.action.capture' => new CaptureAction(),
+            'payum.action.sync' => new SyncAction(),
+            'payum.action.status' => new StatusAction(),
+        ));
+
+        if (false == $config['payum.api']) {
+            $config['options.required'] = array('client_id', 'client_secret', 'config_path');
+            $config['payum.api'] = function (ArrayObject $config) {
+                $config->validateNotEmpty($config['options.required']);
+
+                define("PP_CONFIG_PATH", $config['config_path']);
+                $credential = new OAuthTokenCredential($config['client_id'], $config['client_secret']);
+                $config['payum.api'] = new ApiContext($credential);
+            };
+        }
+
+        return (array) $config;
     }
 }
