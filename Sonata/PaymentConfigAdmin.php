01f67f1d7efcbcf4e256426b5dc6b1b643dffdd9
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-12-30 20:48:27 +0000

    wip

--- Sonata/PaymentConfigAdmin.php
+++ Sonata/PaymentConfigAdmin.php
@@ -2,7 +2,62 @@
 namespace Payum\Bundle\PayumBundle\Sonata;
 
 use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\FormFactoryInterface;
 
-class CampaignAdmin extends Admin
+class PaymentConfigAdmin extends Admin
 {
+    /**
+     * @var FormFactoryInterface
+     */
+    protected $formFactory;
+
+    /**
+     * @param FormFactoryInterface $formFactory
+     */
+    public function setFormFactory(FormFactoryInterface $formFactory)
+    {
+        $this->formFactory = $formFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form->reorder([]); //hack!
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('paymentName')
+            ->add('factoryName')
+            ->add('config', 'array')
+            ->add('_action', 'actions', [
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ]
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFormBuilder()
+    {
+        $formBuilder = $this->formFactory->createBuilder('payum_payment_config', $this->getSubject(), array(
+            'data_class' => get_class($this->getSubject()),
+        ));
+
+        $this->defineFormBuilder($formBuilder);
+
+        return $formBuilder;
+    }
 }
