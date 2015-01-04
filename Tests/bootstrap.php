e8cb9ea725af3ba8d687c71e01bda4f4750ff9c3
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2015-01-04 12:57:55 +0000

    fix travis.

---
+++ Tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    echo <<<EOM
+You must set up the project dependencies by running the following commands:
+
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+
+EOM;
+
+    exit(1);
+}
