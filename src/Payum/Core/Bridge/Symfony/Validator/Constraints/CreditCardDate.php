a6a48a1f1b12044688da1b54c248ec42d8d59d39
Marc Pantel <pantel.m@gmail.com>
2015-04-03 23:04:39 +0200

    Move Validator from PayumBundle (cherry picked from commit 9c358171288d73dfe005f2324ad759d098e812fb)
    Conflicts:
    
    	src/Payum/Core/Bridge/Symfony/Validator/Constraints/CreditCardDateValidator.php

---
+++ src/Payum/Core/Bridge/Symfony/Validator/Constraints/CreditCardDate.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Marc Pantel <pantel.m@gmail.com>
+ */
+namespace Payum\Core\Bridge\Symfony\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\MissingOptionsException;
+
+/**
+ * CreditCardDate
+ */
+class CreditCardDate extends Constraint
+{
+    public $minMessage = 'validator.credit_card.invalidDate';
+    public $invalidMessage = 'validator.credit_card.invalidDate';
+    public $min;
+
+    public function __construct($options = null)
+    {
+        parent::__construct($options);
+
+        if (null === $this->min) {
+            throw new MissingOptionsException('Either option "min" must be given for constraint ' . __CLASS__, array('min'));
+        }
+
+        if (null !== $this->min) {
+            $this->min = new \DateTime($this->min);
+            $this->min->modify('last day of this month');
+        }
+
+    }
+}
