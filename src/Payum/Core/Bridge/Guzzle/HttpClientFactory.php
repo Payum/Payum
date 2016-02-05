4abcd23360402afe01de938d29fd0d66909c0c02
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-02-05 19:30:57 +0000

    fix paypal express checkout issue.
    Paypal error:14094410:SSL routines:SSL3_READ_BYTES:sslv3 alert handshake failure(35)

--- src/Payum/Core/Bridge/Guzzle/HttpClientFactory.php
+++ src/Payum/Core/Bridge/Guzzle/HttpClientFactory.php
@@ -12,22 +12,11 @@ class HttpClientFactory
      */
     public static function createGuzzle()
     {
-        // Reaction to the ssl3.0 shutdown from paypal
-        // https://www.paypal-community.com/t5/PayPal-Forward/PayPal-Response-to-SSL-3-0-Vulnerability-aka-POODLE/ba-p/891829
-        // http://googleonlinesecurity.blogspot.com/2014/10/this-poodle-bites-exploiting-ssl-30.html
+        $curl = curl_version();
 
         $curlOptions = [
-            //There is a constant for that CURL_SSLVERSION_TLSv1, but it is not present on some versions of php.
-            CURLOPT_SSLVERSION => 1,
+            CURLOPT_USERAGENT => sprintf('Payum/1.x curl/%s PHP/%s', $curl['version'], phpversion()),
         ];
-
-        // Do not use the Cipher List for NSS
-        // https://github.com/paypal/sdk-core-php/blob/namespace-5.3/lib/PayPal/Core/PPHttpConfig.php#L51
-        $curl = curl_version();
-        $sslVersion = isset($curl['ssl_version']) ? $curl['ssl_version'] : '';
-        if (false === strpos($sslVersion, "NSS/")) {
-            $curlOptions[CURLOPT_SSL_CIPHER_LIST] = 'TLSv1';
-        }
 
         return new GuzzleClient([
             'curl' => $curlOptions
