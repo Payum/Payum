a792e4a603c6a67efa5ba47d4c7d070a3b0fc8f4
Dmitry <hellsigner@gmail.com>
2016-04-01 15:35:26 +0300

    Fix GatewayFactory; fix require-dev

--- src/Payum/Paypal/ExpressCheckout/Nvp/PaypalExpressCheckoutPermissionGatewayFactory.php
+++ src/Payum/Paypal/ExpressCheckout/Nvp/PaypalExpressCheckoutPermissionGatewayFactory.php
@@ -39,32 +39,34 @@ class PaypalExpressCheckoutPermissionGatewayFactory extends PaypalExpressCheckou
             'payum.factory_title' => 'PayPal ExpressCheckout via merchant token',
         ));
 
-        $config['payum.default_options'] = array(
-            'username' => '',
-            'password' => '',
-            'signature' => '',
-            'token' => '',
-            'tokenSecret' => '',
-            'third_party_subject' => '',
-            'sandbox' => true,
-        );
-        $config->defaults($config['payum.default_options']);
-        $config['payum.required_options'] = array('username', 'password', 'signature', 'token', 'tokenSecret', 'third_party_subject');
-
-        $config['payum.api'] = function (ArrayObject $config) {
-            $config->validateNotEmpty($config['payum.required_options']);
-
-            $paypalConfig = array(
-                'username' => $config['username'],
-                'password' => $config['password'],
-                'signature' => $config['signature'],
-                'token' => $config['token'],
-                'tokenSecret' => $config['tokenSecret'],
-                'third_party_subject' => $config['third_party_subject'],
-                'sandbox' => $config['sandbox'],
+        if (false == $config['payum.api']) {
+            $config['payum.default_options'] = array(
+                'username' => '',
+                'password' => '',
+                'signature' => '',
+                'token' => '',
+                'tokenSecret' => '',
+                'third_party_subject' => '',
+                'sandbox' => true,
             );
+            $config->defaults($config['payum.default_options']);
+            $config['payum.required_options'] = array('username', 'password', 'signature', 'token', 'tokenSecret', 'third_party_subject');
 
-            return new ApiPermission($paypalConfig, $config['payum.http_client'], $config['httplug.message_factory']);
-        };
+            $config['payum.api'] = function (ArrayObject $config) {
+                $config->validateNotEmpty($config['payum.required_options']);
+
+                $paypalConfig = array(
+                    'username' => $config['username'],
+                    'password' => $config['password'],
+                    'signature' => $config['signature'],
+                    'token' => $config['token'],
+                    'tokenSecret' => $config['tokenSecret'],
+                    'third_party_subject' => $config['third_party_subject'],
+                    'sandbox' => $config['sandbox'],
+                );
+
+                return new ApiPermission($paypalConfig, $config['payum.http_client'], $config['httplug.message_factory']);
+            };
+        }
     }
 }
