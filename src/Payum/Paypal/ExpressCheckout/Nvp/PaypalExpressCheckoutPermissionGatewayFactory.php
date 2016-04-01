4f8c1bb692ae6158ea148bfa040d1d40696151ce
Dmitry <hellsigner@gmail.com>
2016-04-01 12:25:11 +0300

    Overrode only API creation logics in PaypalExpressCheckoutPermissionGatewayFactory

--- src/Payum/Paypal/ExpressCheckout/Nvp/PaypalExpressCheckoutPermissionGatewayFactory.php
+++ src/Payum/Paypal/ExpressCheckout/Nvp/PaypalExpressCheckoutPermissionGatewayFactory.php
@@ -3,7 +3,6 @@ namespace Payum\Paypal\ExpressCheckout\Nvp;
 
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\InvalidArgumentException;
-use Payum\Core\GatewayFactory;
 use Payum\Paypal\ExpressCheckout\Nvp\Action\Api\ConfirmOrderAction;
 use Payum\Paypal\ExpressCheckout\Nvp\Action\Api\CreateRecurringPaymentProfileAction;
 use Payum\Paypal\ExpressCheckout\Nvp\Action\Api\DoCaptureAction;
@@ -28,82 +27,44 @@ use Payum\Paypal\ExpressCheckout\Nvp\Action\RecurringPaymentDetailsStatusAction;
 use Payum\Paypal\ExpressCheckout\Nvp\Action\RecurringPaymentDetailsSyncAction;
 use Payum\Paypal\ExpressCheckout\Nvp\ApiPermission;
 
-class PaypalExpressCheckoutPermissionGatewayFactory extends GatewayFactory
+class PaypalExpressCheckoutPermissionGatewayFactory extends PaypalExpressCheckoutGatewayFactory
 {
     /**
      * {@inheritDoc}
      */
     protected function populateConfig(ArrayObject $config)
     {
+        parent::populateConfig($config);
         $config->defaults(array(
-            'payum.factory_name' => 'paypal_express_checkout_nvp',
             'payum.factory_title' => 'PayPal ExpressCheckout via merchant token',
-
-            'payum.template.confirm_order' => '@PayumPaypalExpressCheckout/confirmOrder.html.twig',
-
-            'payum.action.capture' => new CaptureAction(),
-            'payum.action.authorize' => new AuthorizeAction(),
-            'payum.action.convert_payment' => new ConvertPaymentAction(),
-            'payum.action.notify' => new NotifyAction(),
-            'payum.action.status' => new PaymentDetailsStatusAction(),
-            'payum.action.sync' => new PaymentDetailsSyncAction(),
-            'payum.action.recurring_status' => new RecurringPaymentDetailsStatusAction(),
-            'payum.action.recurring_sync' => new RecurringPaymentDetailsSyncAction(),
-
-            'payum.action.api.set_express_checkout' => new SetExpressCheckoutAction(),
-            'payum.action.api.get_express_checkout_details' => new GetExpressCheckoutDetailsAction(),
-            'payum.action.api.get_transaction_details' => new GetTransactionDetailsAction(),
-            'payum.action.api.do_express_checkout_payment' => new DoExpressCheckoutPaymentAction(),
-            'payum.action.api.create_recurring_payment_profile' => new CreateRecurringPaymentProfileAction(),
-            'payum.action.api.update_recurring_payment_profile' => new UpdateRecurringPaymentProfileAction(),
-            'payum.action.api.get_recurring_payments_profile_details' => new GetRecurringPaymentsProfileDetailsAction(),
-            'payum.action.api.cancel_recurring_payments_profile' => new CancelRecurringPaymentsProfileAction(),
-            'payum.action.api.manage_recurring_payments_profile_status' => new ManageRecurringPaymentsProfileStatusAction(),
-            'payum.action.api.create_billing_agreement' => new CreateBillingAgreementAction(),
-            'payum.action.api.do_reference_transaction' => new DoReferenceTransactionAction(),
-            'payum.action.api.do_capture' => new DoCaptureAction(),
-            'payum.action.api.authorize_token' => new AuthorizeTokenAction(),
-            'payum.action.api.confirm_order' => function (ArrayObject $config) {
-                return new ConfirmOrderAction($config['payum.template.confirm_order']);
-            },
         ));
 
-        if (false == $config['payum.api']) {
-            $config['payum.default_options'] = array(
-                'username' => '',
-                'password' => '',
-                'signature' => '',
-                'token' => '',
-                'tokenSecret' => '',
-                'third_party_subject' => '',
-                'sandbox' => true,
+        $config['payum.default_options'] = array(
+            'username' => '',
+            'password' => '',
+            'signature' => '',
+            'token' => '',
+            'tokenSecret' => '',
+            'third_party_subject' => '',
+            'sandbox' => true,
+        );
+        $config->defaults($config['payum.default_options']);
+        $config['payum.required_options'] = array('username', 'password', 'signature', 'token', 'tokenSecret', 'third_party_subject');
+
+        $config['payum.api'] = function (ArrayObject $config) {
+            $config->validateNotEmpty($config['payum.required_options']);
+
+            $paypalConfig = array(
+                'username' => $config['username'],
+                'password' => $config['password'],
+                'signature' => $config['signature'],
+                'token' => $config['token'],
+                'tokenSecret' => $config['tokenSecret'],
+                'third_party_subject' => $config['third_party_subject'],
+                'sandbox' => $config['sandbox'],
             );
-            $config->defaults($config['payum.default_options']);
-            $config['payum.required_options'] = array('username', 'password', 'signature');
 
-            $config['payum.api'] = function (ArrayObject $config) {
-                $config->validateNotEmpty($config['payum.required_options']);
-
-                $paypalConfig = array(
-                    'username' => $config['username'],
-                    'password' => $config['password'],
-                    'signature' => $config['signature'],
-                    'token' => $config['token'],
-                    'tokenSecret' => $config['tokenSecret'],
-                    'third_party_subject' => $config['third_party_subject'],
-                    'sandbox' => $config['sandbox'],
-                );
-
-                if (empty($config['token']) || empty($config['tokenSecret']) || empty($config['third_party_subject'])) {
-                    throw new InvalidArgumentException('"token", "tokenSecret" and "third_party_subject" options must be defined to use third-party token authorization');
-                }
-
-                return new ApiPermission($paypalConfig, $config['payum.http_client'], $config['httplug.message_factory']);
-            };
-        }
-
-        $config['payum.paths'] = array_replace([
-            'PayumPaypalExpressCheckout' => __DIR__.'/Resources/views',
-        ], $config['payum.paths'] ?: []);
+            return new ApiPermission($paypalConfig, $config['payum.http_client'], $config['httplug.message_factory']);
+        };
     }
 }
