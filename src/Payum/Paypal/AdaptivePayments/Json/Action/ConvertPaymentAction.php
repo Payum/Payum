b4565bd05663dc97cfddcbc81cb9a5ff46bba097
Dmitry <hellsigner@gmail.com>
2016-04-29 15:57:03 +0300

    Added ConvertPayment action (experimental)

---
+++ src/Payum/Paypal/AdaptivePayments/Json/Action/ConvertPaymentAction.php
@@ -0,0 +1,52 @@
+<?php
+namespace Payum\Paypal\AdaptivePayments\Json\Action\Api;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\Model\PaymentInterface;
+use Payum\Core\Request\Convert;
+use Payum\Core\Request\GetCurrency;
+
+class ConvertPaymentAction implements ActionInterface, GatewayAwareInterface
+{
+    use GatewayAwareTrait;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param Convert $request
+     */
+    public function execute($request)
+    {
+        RequestNotSupportedException::assertSupports($this, $request);
+
+        /** @var PaymentInterface $payment */
+        $payment = $request->getSource();
+
+        $this->gateway->execute($currency = new GetCurrency($payment->getCurrencyCode()));
+        $divisor = pow(10, $currency->exp);
+
+        $details = ArrayObject::ensureArrayObject($payment->getDetails());
+        $details['invNum'] = $payment->getNumber();
+        $details['currencyCode'] = $payment->getCurrencyCode();
+        $details['totalAmount'] = $payment->getTotalAmount() / $divisor;
+        $details['description'] = $payment->getDescription();
+        
+        $request->setResult((array) $details);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($request)
+    {
+        return
+            $request instanceof Convert &&
+            $request->getSource() instanceof PaymentInterface &&
+            $request->getTo() == 'array'
+        ;
+    }
+}
