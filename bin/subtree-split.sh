87e3beebd40b41f54bfb95521b9a60e00b481733
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-07-13 11:24:52 +0300

    Release 1.3.5

--- bin/subtree-split.sh
+++ bin/subtree-split.sh
@@ -8,7 +8,7 @@ sleep 3
 
 function split()
 {
-    git subtree push --prefix=$1 $2 $CURRENT_BRANCH
+    #git subtree push --prefix=$1 $2 $CURRENT_BRANCH
     sleep 2
 }
 
@@ -39,18 +39,18 @@ remote stripe git@github.com:Payum/Stripe.git
 remote sofort git@github.com:Payum/Sofort.git
 remote skeleton git@github.com:Payum/Skeleton.git
 
-split 'src/Payum/Core/' core
-split 'src/Payum/AuthorizeNet/Aim/' authorize-net-aim
-split 'src/Payum/Be2Bill' be2bill
-split 'src/Payum/Paypal/ExpressCheckout/Nvp' paypal-express-checkout
-split 'src/Payum/Paypal/Masspay/Nvp' paypal-masspay
-split 'src/Payum/Paypal/ProCheckout/Nvp' paypal-pro-checkout
-split 'src/Payum/Paypal/Ipn' paypal-ipn
-split 'src/Payum/Paypal/Rest' paypal-rest
-split 'src/Payum/Offline' offline
-split 'src/Payum/Payex' payex
-split 'src/Payum/Klarna/Checkout' klarna-checkout
-split 'src/Payum/Klarna/Invoice' klarna-invoice
-split 'src/Payum/Sofort' sofort
-split 'src/Payum/Stripe' stripe
-split 'src/Payum/Skeleton' skeleton
+forceSplit 'src/Payum/Core/' core
+#split 'src/Payum/AuthorizeNet/Aim/' authorize-net-aim
+#split 'src/Payum/Be2Bill' be2bill
+#split 'src/Payum/Paypal/ExpressCheckout/Nvp' paypal-express-checkout
+#split 'src/Payum/Paypal/Masspay/Nvp' paypal-masspay
+#split 'src/Payum/Paypal/ProCheckout/Nvp' paypal-pro-checkout
+#split 'src/Payum/Paypal/Ipn' paypal-ipn
+#split 'src/Payum/Paypal/Rest' paypal-rest
+#split 'src/Payum/Offline' offline
+#split 'src/Payum/Payex' payex
+#split 'src/Payum/Klarna/Checkout' klarna-checkout
+#split 'src/Payum/Klarna/Invoice' klarna-invoice
+#split 'src/Payum/Sofort' sofort
+#split 'src/Payum/Stripe' stripe
+#split 'src/Payum/Skeleton' skeleton
