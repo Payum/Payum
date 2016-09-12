23620bccee9a847d1c852d745ab71b9e2495696f
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-09-12 20:40:13 +0300

    Reuse splitsh-lite tool to split main repo.

--- bin/subtree-split.sh
+++ bin/subtree-split.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-#set -e
+set -e
 set -x
 
 CURRENT_BRANCH=`git name-rev --name-only HEAD`
-sleep 3
 
 function split()
 {
-    #git subtree push --prefix=$1 $2 $CURRENT_BRANCH
-    sleep 2
-}
+    CURRENT_BRANCH=`git name-rev --name-only HEAD`
 
-function forceSplit()
-{
-    git push $2 `git subtree split --prefix $1 $CURRENT_BRANCH`:$CURRENT_BRANCH --force
-    sleep 2
+	SHA1=`~/.golibs/splitsh-lite --prefix=$1`
+    git push $2 "$SHA1:$CURRENT_BRANCH"
 }
 
 function remote()
 {
-    git remote add $1 $2
+    git remote add $1 $2 || true
 }
 
 remote core git@github.com:Payum/Core.git
@@ -39,18 +34,18 @@ remote stripe git@github.com:Payum/Stripe.git
 remote sofort git@github.com:Payum/Sofort.git
 remote skeleton git@github.com:Payum/Skeleton.git
 
-forceSplit 'src/Payum/Core/' core
-#split 'src/Payum/AuthorizeNet/Aim/' authorize-net-aim
-#split 'src/Payum/Be2Bill' be2bill
-#split 'src/Payum/Paypal/ExpressCheckout/Nvp' paypal-express-checkout
-#split 'src/Payum/Paypal/Masspay/Nvp' paypal-masspay
-#split 'src/Payum/Paypal/ProCheckout/Nvp' paypal-pro-checkout
-#split 'src/Payum/Paypal/Ipn' paypal-ipn
-#split 'src/Payum/Paypal/Rest' paypal-rest
-#split 'src/Payum/Offline' offline
-#split 'src/Payum/Payex' payex
-#split 'src/Payum/Klarna/Checkout' klarna-checkout
-#split 'src/Payum/Klarna/Invoice' klarna-invoice
-#split 'src/Payum/Sofort' sofort
-#split 'src/Payum/Stripe' stripe
-#split 'src/Payum/Skeleton' skeleton
+split 'src/Payum/Core/' core
+split 'src/Payum/AuthorizeNet/Aim/' authorize-net-aim
+split 'src/Payum/Be2Bill' be2bill
+split 'src/Payum/Paypal/ExpressCheckout/Nvp' paypal-express-checkout
+split 'src/Payum/Paypal/Masspay/Nvp' paypal-masspay
+split 'src/Payum/Paypal/ProCheckout/Nvp' paypal-pro-checkout
+split 'src/Payum/Paypal/Ipn' paypal-ipn
+split 'src/Payum/Paypal/Rest' paypal-rest
+split 'src/Payum/Offline' offline
+split 'src/Payum/Payex' payex
+split 'src/Payum/Klarna/Checkout' klarna-checkout
+split 'src/Payum/Klarna/Invoice' klarna-invoice
+split 'src/Payum/Sofort' sofort
+split 'src/Payum/Stripe' stripe
+split 'src/Payum/Skeleton' skeleton
