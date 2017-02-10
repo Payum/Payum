1a59d7d527eae74eecaa03a61314cdda9b5b50d7
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-02-10 11:54:02 +0200

    Correct description for catchReply argument.
    https://github.com/Payum/Payum/pull/626

--- src/Payum/Core/GatewayInterface.php
+++ src/Payum/Core/GatewayInterface.php
@@ -5,10 +5,10 @@ interface GatewayInterface
 {
     /**
      * @param mixed   $request
-     * @param boolean $catchReply
+     * @param boolean $catchReply If false the reply behave like an exception. If true the reply will be caught internally and returned.
      *
-     * @throws \Payum\Core\Exception\RequestNotSupportedException if there is not an action which able to process the request.
-     * @throws \Payum\Core\Reply\ReplyInterface                   when a gateway needs some external tasks to be executed. like a redirect to a gateway site or a page with credit card form. if $catchReply set to false the reply will be returned.
+     * @throws \Payum\Core\Exception\RequestNotSupportedException If there is not an action which able to process the request.
+     * @throws \Payum\Core\Reply\ReplyInterface                   Gateway throws reply if some external tasks have to be done. For example show a credit card form, an iframe or perform a redirect.
      *
      * @return \Payum\Core\Reply\ReplyInterface|null
      */
