053ec91207a66fb1d0c01c06f342efd50e8603e9
Liam Sorsby <liam.sorsby@skybettingandgaming.com>
2019-03-03 12:51:40 +0000

    ADDED security check to run after install

--- src/Payum/Stripe/StripeJsGatewayFactory.php
+++ src/Payum/Stripe/StripeJsGatewayFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Payum\Stripe;
 
 use Payum\Core\Bridge\Spl\ArrayObject;
@@ -10,12 +11,13 @@ class StripeJsGatewayFactory extends StripeCheckoutGatewayFactory
      */
     protected function populateConfig(ArrayObject $config)
     {
-        $config->defaults(array(
-            'payum.factory_name' => 'stripe_js',
-            'payum.factory_title' => 'Stripe.Js',
-
-            'payum.template.obtain_token' => '@PayumStripe/Action/obtain_js_token.html.twig',
-        ));
+        $config->defaults(
+            [
+                'payum.factory_name' => 'stripe_js',
+                'payum.factory_title' => 'Stripe.Js',
+                'payum.template.obtain_token' => '@PayumStripe/Action/obtain_js_token.html.twig',
+            ]
+        );
 
         parent::populateConfig($config);
     }
