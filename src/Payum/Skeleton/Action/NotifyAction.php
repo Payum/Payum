2558ced6d90aabff2e35930b2f528ca178a3e97d
Hugo Alliaume <kocal@live.fr>
2019-03-15 09:49:12 +0100

    doc(skeleton): add code examples in NotifyAction

--- src/Payum/Skeleton/Action/NotifyAction.php
+++ src/Payum/Skeleton/Action/NotifyAction.php
@@ -22,6 +22,23 @@ class NotifyAction implements ActionInterface
 
         $model = ArrayObject::ensureArrayObject($request->getModel());
 
+        // This action is called when calling: $gateway->execute(new Notify($token))
+        // See: https://github.com/Payum/Payum/blob/master/docs/examples/notify-script.md
+        //
+        // If you created a notification token and you sent the notification URL to the bank,
+        // the bank should call this URL at different steps of the payment (e.g.: when payment is successful).
+        //
+        // The simplest logic to implement here is to take GET or POST parameters and use them to replace your model:
+
+        /*
+        $this->gateway->execute($httpRequest = new GetHttpRequest());
+
+        $model->replace($httpRequest->request); // takes POST parameters
+        $model->replace($httpRequest->request); // or taks GET parameters
+
+        throw new HttpResponse('OK', 200);
+        */
+
         throw new \LogicException('Not implemented');
     }
 
