62fc6d5a02af6138ec418e825d8e26e258dab9e5
Pierre du Plessis <pierre@pcservice.co.za>
2022-05-05 16:40:10 +0200

    Add Rector

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src'
+    ]);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
+        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
+        PHPUnitSetList::PHPUNIT_EXCEPTION,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::REMOVE_MOCKS,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_100
+    ]);
+
+    $services = $rectorConfig->services();
+
+    $services->remove(AddDoesNotPerformAssertionToNonAssertingTestRector::class);
+    $services->remove(AddSeeTestAnnotationRector::class);
+};
