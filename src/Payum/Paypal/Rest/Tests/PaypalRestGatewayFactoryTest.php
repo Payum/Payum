1e8881446ca677edd0acdb2248c96abab9936dc6
Pierre du Plessis <pierre@pcservice.co.za>
2022-05-05 16:46:29 +0200

    Fix regex check

--- src/Payum/Paypal/Rest/Tests/PaypalRestGatewayFactoryTest.php
+++ src/Payum/Paypal/Rest/Tests/PaypalRestGatewayFactoryTest.php
@@ -136,7 +136,7 @@ class PaypalRestGatewayFactoryTest extends AbstractGatewayFactoryTest
         if (method_exists($this, 'expectExceptionMessageMatches')) {
             $this->expectExceptionMessageMatches('/Given \"config_path\" is invalid. \w+/');
         } else {
-            $this->expectExceptionMessageMatches('/Given \"config_path\" is invalid. \w+/');
+            $this->expectExceptionMessageRegExp('/Given \"config_path\" is invalid. \w+/');
         }
 
         $factory = new PaypalRestGatewayFactory();
