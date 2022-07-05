deff64283b5e73fb23c2007564ef50630375de3f
Pierre du Plessis <pierre@pcservice.co.za>
2022-07-05 17:29:48 +0200

    Add EasyCodingStandard

---
+++ ecs.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([__DIR__ . '/src']);
+    $ecsConfig->sets([]);
+};
