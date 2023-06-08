59c857d81dfc8b78c0bbcbc1790932406765e663
Pierre du Plessis <pierre@pcservice.co.za>
2023-06-08 15:29:33 +0200

    Check that apiClass is a string before passing it to class_exists

--- src/Payum/Core/ApiAwareTrait.php
+++ src/Payum/Core/ApiAwareTrait.php
@@ -12,7 +12,7 @@ trait ApiAwareTrait
     protected $api;
 
     /**
-     * @var string
+     * @var string|object
      */
     protected $apiClass;
 
@@ -24,11 +24,12 @@ trait ApiAwareTrait
         if (empty($this->apiClass)) {
             throw new LogicException(sprintf('You must configure apiClass in __constructor method of the class the trait is applied to.'));
         }
-        if (false == (class_exists($this->apiClass) || interface_exists($this->apiClass))) {
+
+        if (is_string($this->apiClass) && !(class_exists($this->apiClass) || interface_exists($this->apiClass))) {
             throw new LogicException(sprintf('Api class not found or invalid class. "%s", $this->apiClass', $this->apiClass));
         }
-        
-        if (false == $api instanceof $this->apiClass) {
+
+        if (!$api instanceof $this->apiClass) {
             throw new UnsupportedApiException(sprintf('Not supported api given. It must be an instance of %s', $this->apiClass));
         }
 
