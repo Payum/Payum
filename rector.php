c91c2da86f10c12516b4b71d8d0f029ef1b5e330
Pierre du Plessis <pierre@pcservice.co.za>
2023-11-06 09:38:21 +0200

    Add PHPUnit 9 and update PHPUnit and Rector config

--- rector.php
+++ rector.php
@@ -9,6 +9,10 @@ use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Php71\Rector\ClassConst\PublicConstantVisibilityRector;
 use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -21,7 +25,17 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->phpVersion(PhpVersion::PHP_72);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_72
+        LevelSetList::UP_TO_PHP_72,
+
+        // PHPUnit
+        PHPUnitSetList::PHPUNIT_40,
+        PHPUnitSetList::PHPUNIT_50,
+        PHPUnitSetList::PHPUNIT_60,
+        PHPUnitSetList::PHPUNIT_70,
+        PHPUnitSetList::PHPUNIT_80,
+        PHPUnitSetList::PHPUNIT_90,
+        PHPUnitSetList::PHPUNIT_100,
+        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
     ]);
 
     $rectorConfig->skip([
@@ -29,5 +43,9 @@ return static function (RectorConfig $rectorConfig): void {
         StringClassNameToClassConstantRector::class,
         RemoveExtraParametersRector::class,
         PublicConstantVisibilityRector::class,
+
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
+        AddSeeTestAnnotationRector::class,
+        RenameClassRector::class,
     ]);
 };
