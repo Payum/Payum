7bfc0b62de407f075723dd2e2e8848ca689d9b61
Pierre du Plessis <pierre@pcservice.co.za>
2026-08-13 09:58:59 +0200

    Add an engine-agnostic template renderer

---
+++ src/Payum/Core/Tests/Bridge/Twig/TwigRendererTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Payum\Core\Tests\Bridge\Twig;
+
+use Payum\Core\Bridge\Twig\TwigRenderer;
+use Payum\Core\Template\RendererInterface;
+use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use Twig\Loader\ChainLoader;
+
+final class TwigRendererTest extends TestCase
+{
+    public function testShouldImplementRendererInterface(): void
+    {
+        $renderer = new TwigRenderer($this->twig(), '@PayumCore/layout.html.twig');
+
+        $this->assertInstanceOf(RendererInterface::class, $renderer);
+    }
+
+    public function testShouldRenderATemplateWithItsContext(): void
+    {
+        $renderer = new TwigRenderer($this->twig([
+            'form.html.twig' => 'Pay {{ amount }}',
+        ]), '@PayumCore/layout.html.twig');
+
+        $this->assertSame('Pay 123', $renderer->render('form.html.twig', [
+            'amount' => 123,
+        ]));
+    }
+
+    public function testShouldMakeTheLayoutAvailableToTheTemplate(): void
+    {
+        $renderer = new TwigRenderer($this->twig([
+            'form.html.twig' => '{{ layout }}',
+        ]), '@PayumCore/layout.html.twig');
+
+        $this->assertSame('@PayumCore/layout.html.twig', $renderer->render('form.html.twig'));
+    }
+
+    public function testShouldLetTheContextOverrideTheLayout(): void
+    {
+        $renderer = new TwigRenderer($this->twig([
+            'form.html.twig' => '{{ layout }}',
+        ]), '@PayumCore/layout.html.twig');
+
+        $this->assertSame('@Acme/layout.html.twig', $renderer->render('form.html.twig', [
+            'layout' => '@Acme/layout.html.twig',
+        ]));
+    }
+
+    public function testShouldRegisterThePathsItWasGiven(): void
+    {
+        $renderer = new TwigRenderer(
+            $this->twig(),
+            '@PayumCore/layout.html.twig',
+            [
+                'PayumCore' => __DIR__ . '/../../../Resources/views',
+            ],
+        );
+
+        $this->assertStringContainsString(
+            '<!DOCTYPE html>',
+            $renderer->render('@PayumCore/layout.html.twig'),
+        );
+    }
+
+    /**
+     * @param array<string, string> $templates
+     */
+    private function twig(array $templates = []): Environment
+    {
+        return new Environment(new ChainLoader([new ArrayLoader($templates)]));
+    }
+}
